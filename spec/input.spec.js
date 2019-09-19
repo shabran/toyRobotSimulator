17120437858572a5fba41ceb0e9f7f8f33ebfe31
unknown <shabran.ahmad@gmail.com>
2019-09-19 20:22:43 +0700

    added input.spec.js

---
+++ spec/input.spec.js
@@ -0,0 +1,18 @@
+describe("Input", function() {
+const Input=require('../services/input');
+var input;
+var filePath = "testFile/test.txt";
+
+ beforeEach(()=> {
+    input = new Input(filePath);
+  });
+
+  it("Should have at least 3 instructions", ()=> {
+    expect(input.commands.length).toBeGreaterThanOrEqual(3);
+  });
+
+  it("Should have PLACE as first command", () =>{
+    expect(input.FirstCommandCheck()).toBeTruthy;
+  });
+
+});
