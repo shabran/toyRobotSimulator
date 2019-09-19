52febe06820c021e5356e09ffdc1eaec701a2905
unknown <shabran.ahmad@gmail.com>
2019-09-19 16:43:24 +0700

    updated app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let args = process.argv[2];
-console.log(args);
+
 const TableTop = require('./tableTop'),
     Input = require('./input'),
     Robot = require('./robot');
@@ -7,43 +7,46 @@ const TableTop = require('./tableTop'),
 let tableTop = new TableTop(5, 5);
 let robot = new Robot();
 
-let fileUrl = args;
-let input = new Input(fileUrl);
 
-if (input.commands) {
+if(args.length > 0){
+    let fileUrl = args;
+    let input = new Input(fileUrl);
+    if(input.commands && input.FirstCommandCheck()){
+        for( i = 0; i < input.commands.length; i++){
+            let command = input.commands[i];
 
-    for (i = 0; i < input.commands.length; i++) {
-        let command = input.commands[i];
-
-        switch (input.getCommandWithoutArguments(command)) {
-            case 'PLACE':
-                let placeArguments = input.getCommandArguments(command);
-                let xPos = placeArguments[0];
-                let yPos = placeArguments[1];
-                let facing = placeArguments[2];
-
-                //Robot is not in the table 
-                if ((xPos >= tableTop.xSize) && (yPos >= tableTop.ySize)) {
-                    console.error("Robot position out of table top");
-                    return;
-                }
-
-                robot.place(xPos, yPos, facing);
-                break;
-            case 'MOVE':
-                robot.move(tableTop);
-                break;
-            case 'LEFT':
-            case 'RIGHT':
-                robot.rotate(command);
-                break;
-            case 'REPORT':
-                console.log("\n\nOutput:" + robot.x + "," + robot.y + "," + robot.facing + "\n\n");
-                break;
-            default:
+            switch(input.getCommandWithoutArguments(command)){
+                case 'PLACE':
+                    let placeArguments = input.getCommandArguments(command);
+                    let xPos = placeArguments[0];
+                    let yPos = placeArguments[1];
+                    let facing = placeArguments[2];
+                    
+                    //wrong location, robot not on the table 
+                    if((xPos >= tableTop.xSize) && (yPos >= tableTop.ySize)){
+                        console.error("Robot position out of table top");
+                        return;
+                    }
+                    robot.place(xPos, yPos, facing);
+                    break;
+                case 'MOVE':
+                    robot.move(tableTop);
+                    break;
+                case 'LEFT':
+                case 'RIGHT':
+                    robot.rotate(command);
+                    break;
+                case 'REPORT':
+                    console.log("\n\nOutput:" + robot.x + "," + robot.y + "," + robot.facing + "\n\n");
+                    break;
+                default:
+            }
         }
     }
-
-} else {
-    console.log("Error");
+    else{
+        console.log("\n\nError: First command should be PLACE command\n\n");
+    }
+}
+else{
+    console.log("\n\nError: Missing command file path\n\n");
 }
